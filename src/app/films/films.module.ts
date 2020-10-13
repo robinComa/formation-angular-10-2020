187214feca4e5790d274f8747e563b4262eba06f
Robin Coma Delperier <robin@placeme.io>
2020-10-13 14:25:51 +0200

    feat(Film): film list

--- src/app/films/films.module.ts
+++ src/app/films/films.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmComponent } from './film/film.component';
+import { FilmListComponent } from './film-list/film-list.component';
 
 @NgModule({
-  declarations: [FilmComponent],
+  declarations: [FilmComponent, FilmListComponent],
   imports: [
     CommonModule
   ],
-  exports: [FilmComponent]
+  exports: [FilmComponent, FilmListComponent]
 })
 export class FilmsModule { }
