29688d6938aa38ccd6702fc292493a899cf995a5
Robin Coma Delperier <robin@placeme.io>
2020-10-13 14:08:20 +0200

    feat(Film): app-film template

--- src/app/films/film/film.component.ts
+++ src/app/films/film/film.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../shared/film';
 
 @Component({
   selector: 'app-film',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmComponent implements OnInit {
 
+  film: Film;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.film = {
+      titre: 'OSS 117 : Alerte rouge en Afrique noire',
+      description: '1981. Hubert Bonisseur de La Bath, alias OSS 117, est de retour. Pour cette nouvelle mission, plus délicate, plus périlleuse et plus torride que jamais, il est contraint de faire équipe avec un jeune collègue, le prometteur OSS 1001.',
+      image: 'https://fr.web.img5.acsta.net/pictures/20/09/10/18/05/4458149.jpg'
+    };
   }
 
 }
---
+++ src/app/films/shared/film.ts
@@ -0,0 +1,5 @@
+export interface Film {
+  titre: string;
+  description: string;
+  image: string;
+}
